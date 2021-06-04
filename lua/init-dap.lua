c6e72e372f8855af4fdcf948939c048310c14278
Adam Wolski <adam@wolski.email>
2021-06-04 11:33:33 +0200

    [CodeQuality, DAP] Use do-end for scoping variables

--- lua/init-dap.lua
+++ lua/init-dap.lua
@@ -1,27 +1,29 @@
-local dap = require"dap"
+do
+  local dap = require"dap"
 
-dap.configurations.lua = { 
-  { 
-    type = 'nlua', 
-    request = 'attach',
-    name = "Attach to running Neovim instance",
-    host = function()
-      local value = vim.fn.input('Host [127.0.0.1]: ')
-      if value ~= "" then
-        return value
-      end
-      return '127.0.0.1'
-    end,
-    port = function()
-      local val = tonumber(vim.fn.input('Port: '))
-      assert(val, "Please provide a port number")
-      return val
-    end,
+  dap.configurations.lua = {
+    {
+      type = 'nlua',
+      request = 'attach',
+      name = "Attach to running Neovim instance",
+      host = function()
+        local value = vim.fn.input('Host [127.0.0.1]: ')
+        if value ~= "" then
+          return value
+        end
+        return '127.0.0.1'
+      end,
+      port = function()
+        local val = tonumber(vim.fn.input('Port: '))
+        assert(val, "Please provide a port number")
+        return val
+      end,
+    }
   }
-}
 
-dap.adapters.nlua = function(callback, config)
-  callback({ type = 'server', host = config.host, port = config.port })
+  dap.adapters.nlua = function(callback, config)
+    callback({ type = 'server', host = config.host, port = config.port })
+  end
 end
 
 vim.g.dap_virtual_text = true
@@ -63,16 +65,18 @@ require"dapui".setup({
   }
 )
 
-local function set_keymap(lhs, rhs) 
-  local keymap_opts = { noremap=true, silent=true }
-  vim.api.nvim_set_keymap('n', lhs, rhs, keymap_opts) 
-end
+do
+  local function set_keymap(lhs, rhs)
+    local keymap_opts = { noremap=true, silent=true }
+    vim.api.nvim_set_keymap('n', lhs, rhs, keymap_opts)
+  end
 
-set_keymap('<F5>',    ":lua require'dap'.continue()<CR>")
-set_keymap('<F10>',   ":lua require'dap'.step_over()<CR>")
-set_keymap('<F11>',   ":lua require'dap'.step_into()<CR>")
-set_keymap('<S-F11>', ":lua require'dap'.step_out()<CR>")
-set_keymap('<F9>',    ":lua require'dap'.toggle_breakpoint()<CR>")
-set_keymap('<S-F9>',  ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-set_keymap('<F12>',   ":lua require'dap'.repl.open()<CR>")
-set_keymap('<S-F12>', ":lua require'dap'.run_last()<CR>")
+  set_keymap('<F5>',    ":lua require'dap'.continue()<CR>")
+  set_keymap('<F10>',   ":lua require'dap'.step_over()<CR>")
+  set_keymap('<F11>',   ":lua require'dap'.step_into()<CR>")
+  set_keymap('<S-F11>', ":lua require'dap'.step_out()<CR>")
+  set_keymap('<F9>',    ":lua require'dap'.toggle_breakpoint()<CR>")
+  set_keymap('<S-F9>',  ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+  set_keymap('<F12>',   ":lua require'dap'.repl.open()<CR>")
+  set_keymap('<S-F12>', ":lua require'dap'.run_last()<CR>")
+end

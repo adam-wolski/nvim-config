1601d623234955c2938042a85963536ba1252c82
Adam Wolski <adam@wolski.email>
2021-06-04 11:32:31 +0200

    [LSP] Add config for lua language server

--- lua/init-lsp.lua
+++ lua/init-lsp.lua
@@ -53,4 +53,36 @@ nvim_lsp.powershell_es.setup {
   on_attach = on_attach,
 }
 
+do
+  local system_name = "Windows" -- (Linux, macOS, or Windows)
+  local sumneko_root_path = os.getenv('LUA_LANGUAGE_SERVER')
+  local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
+  nvim_lsp.sumneko_lua.setup({
+    on_attach = on_attach,
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    -- An example of settings for an LSP server.
+    --    For more options, see nvim-lspconfig
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = vim.split(package.path, ';'),
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = {
+            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          },
+        },
+      }
+    },
+  })
+end

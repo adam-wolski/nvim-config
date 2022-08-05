f74007011e97ff0603d2d56b4f5f5245e7526934
Adam Wolski <adam@wolski.email>
2022-08-05 08:46:41 +0200

    [dap] Update plugin and rust config

--- lua/init-dap.lua
+++ lua/init-dap.lua
@@ -58,55 +58,19 @@ do
   }
 
   dap.configurations.c = dap.configurations.cpp
+  
+  dap.adapters.codelldb = {
+    type = 'server',
+    port = "${port}",
+    executable = {
+      -- CHANGE THIS to your path!
+      command = os.getenv('CODE_LLDB_PATH'),
+      args = {"--port", "${port}"},
 
-  dap.adapters.codelldb = function(on_adapter)
-    local stdout = vim.loop.new_pipe(false)
-    local stderr = vim.loop.new_pipe(false)
-
-    local cmd = os.getenv('CODE_LLDB_PATH')
-
-    local handle, pid_or_err
-    local opts = {
-      stdio = {nil, stdout, stderr},
-      detached = true,
+      -- On windows you may have to uncomment this:
+      -- detached = false,
     }
-    handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
-      stdout:close()
-      stderr:close()
-      handle:close()
-      if code ~= 0 then
-        print("codelldb exited with code", code)
-      end
-    end)
-    assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
-    stdout:read_start(function(err, chunk)
-      assert(not err, err)
-      if chunk then
-        local port = chunk:match('Listening on port (%d+)')
-        if port then
-          vim.schedule(function()
-            on_adapter({
-              type = 'server',
-              host = '127.0.0.1',
-              port = port
-            })
-          end)
-        else
-          vim.schedule(function()
-            require("dap.repl").append(chunk)
-          end)
-        end
-      end
-    end)
-    stderr:read_start(function(err, chunk)
-      assert(not err, err)
-      if chunk then
-        vim.schedule(function()
-          require("dap.repl").append(chunk)
-        end)
-      end
-    end)
-  end
+  }
 
   dap.configurations.rust = {
     {

28ebb3ff88ca5db4e34afb0385a7e3334a1b30d9
Adam Wolski <adam@wolski.email>
2022-08-27 13:23:39 +0200

    [rust] Add toggle inlays shortcut

--- lua/init-rust.lua
+++ lua/init-rust.lua
@@ -1,15 +1,30 @@
 local rt = require("rust-tools")
 
+local inlays = false
+
+local function toggle_inlays()
+  if inlays then
+    rt.inlay_hints.disable()
+    inlays = false
+  else
+    rt.inlay_hints.enable()
+    inlays = true
+  end
+end
+
 rt.setup({
+  tools = {
+    inlay_hints = {
+      auto = false,
+    },
+  },
   server = {
     on_attach = function(client, bufnr)
       require("init-lsp").on_attach(client, bufnr)
 
-      -- Hover actions
       vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-
-      -- Code action groups
       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "<C-h>", toggle_inlays, { buffer = bufnr })
     end,
   },
   dap = {

33bd467a55d76232d6087c000c9f32b694eee5a6
Adam Wolski <adam@wolski.email>
2023-12-26 08:25:48 +0100

    [neovide] Disable all new fxs

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ vim.g.mapleader = [[ ]]
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_cursor_trail_length = 0
 vim.g.neovide_refresh_rate = 144
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_floating_shadow = false
+vim.g.neovide_scroll_animation_far_lines = 0
 vim.g.vimsyn_embed = 'l'
 vim.o.breakat = "),="
 vim.o.grepprg = "rg --vimgrep"

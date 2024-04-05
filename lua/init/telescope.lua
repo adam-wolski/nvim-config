7e0f99bc99bc4ed70dd891fe3baf753efa703c5a
Adam Wolski <adam@wolski.email>
2024-04-05 16:18:20 +0200

    [telescope] Improve path readability

--- lua/init/telescope.lua
+++ lua/init/telescope.lua
@@ -4,5 +4,12 @@ vim.cmd('packadd telescope.nvim')
 vim.cmd('packadd telescope-dap.nvim')
 vim.cmd('packadd telescope-file-browser.nvim')
 
+require('telescope').setup({
+  defaults = {
+    layout_strategy = 'vertical',
+    path_display = {shorten = { len = 2, exclude = { -1 } }},
+  },
+})
+
 require('telescope').load_extension('dap')
 require('telescope').load_extension('file_browser')

3b615ea13d4b1ac53ead84eeeba1c7a3d3bea104
Adam Wolski <adam@wolski.email>
2024-05-23 21:19:48 +0200

    [fix][lsp] Fix progress not showing

--- lua/utils/lsp_progress.lua
+++ lua/utils/lsp_progress.lua
@@ -8,11 +8,7 @@ M.lsp_progress = function()
 	local lsp = vim.lsp.status()
 
 	if lsp then
-		local name = lsp.name or ""
-		local msg = lsp.message or ""
-		local percentage = lsp.percentage or 0
-		local title = lsp.title or ""
-		return string.format(" %%<%s: %s %s (%s%%%%) ", name, title, msg, percentage)
+	  return lsp
 	end
 	return ""
 end

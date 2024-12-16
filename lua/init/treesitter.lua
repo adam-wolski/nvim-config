a005f5f401529b3cfc8974c1b525ebddb80f8b0a
Adam Wolski <adam@wolski.email>
2024-12-16 19:12:01 +0100

    [treesitter] Nu is now a default module

--- lua/init/treesitter.lua
+++ lua/init/treesitter.lua
@@ -22,6 +22,7 @@ require'nvim-treesitter.configs'.setup {
     "lua",
     "luadoc",
     "markdown",
+    "nu",
     "python",
     "toml",
     "typescript",
@@ -108,13 +109,3 @@ require'nvim-treesitter.configs'.setup {
   }
 }
 
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_config.nu = {
-  install_info = {
-    url = "https://github.com/nushell/tree-sitter-nu",
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-  filetype = "nu",
-}

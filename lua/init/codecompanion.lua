3fb0291e7588b3e2b42cd4c71aa848a2b6064e7c
Adam Wolski <adam@wolski.email>
2025-03-25 18:33:41 +0100

    [Plugins] Update codecompanion

--- lua/init/codecompanion.lua
+++ lua/init/codecompanion.lua
@@ -2,7 +2,30 @@ vim.cmd('packadd codecompanion.nvim')
 
 require("codecompanion").setup({
   strategies = {
-    chat = "openai",
-    inline = "openai"
+    chat = {
+      adapter = "anthropic",
+      slash_commands = {
+        ["file"] = {
+          -- Location to the slash command in CodeCompanion
+          callback = "strategies.chat.slash_commands.file",
+          description = "Select a file using Telescope",
+          opts = {
+            provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+            contains_code = true,
+          },
+        },
+      },
+    },
+    inline = {
+      adapter = "anthropic",
+    },
+    cmd = {
+      adapter = "anthropic",
+    }
+  },
+  display = {
+    action_palette = {
+      provider = "telescope", -- default|telescope|mini_pick
+    },
   },
 })
